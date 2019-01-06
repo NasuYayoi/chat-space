d58bbcffe2ffa2771e7882c260c45ceaa55f6bb4
NasuYayoi <ynynyn1014@gmail.com>
2019-01-06 18:38:20 +0900

    Make validation.js
    非同期通信の際にメッセージもイメージファイルも空の場合には送信できないように設定した記述を、application.jsからvalidation
    .jsに移した。
    Application.jsに直接書くのはよくないため。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$(function() {
-  $(document).on('click', 'input[type=submit]', function() {
-    if (($('input[type=text]').val() == '') && ($('input[type=file]').val() == '')){
-      return false;
-    }
-  })
-})
