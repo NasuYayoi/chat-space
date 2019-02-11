6dd18671fd6c4c9100d3f61c8db8b5ef85f4a534
NasuYayoi <ynynyn1014@gmail.com>
2019-02-11 20:58:31 +0900

    Fix: インクリメンタルサーチで表示されるユーザの処理をグループ新規作成時と編集時で条件分岐させた
    user.jsの中で行われるajax通信で送られるデータに対し、GroupIdがある時（グループ編集時）と、ない時（グループ新規作成時）でユーザ
    の検索条件を変えるため、コントローラでif文を使い条件分岐させる。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
 
   def index
-    @group = Group.find(params[:groupId])
-    # @ids = @group.users.ids
-    # @users = User.where.not(id:@ids).where('(name LIKE(?)) and (id != ?)', "%#{params[:keyword]}%", "#{current_user.id}")
-    # 新規作成の時にエラーになるので、グループIDがある時とない時でif文で分岐させる。
-    @users = User.where('(name LIKE(?)) and (id != ?)', "%#{params[:keyword]}%", "#{current_user.id}")
+    # GroupIdがない時（グループ新規作成時）
+    if (params[:groupId] == "")
+      @users = User.where('(name LIKE(?)) and (id != ?)', "%#{params[:keyword]}%", "#{current_user.id}")
+    # GroupIdがある時（グループ編集時）
+    else
+      # 編集するグループのidを変数に入れる
+      @group = Group.find(params[:groupId])
+      # グループに所属するユーザのidを変数に入れる
+      @ids = @group.users.ids
+      # メンバー追加のテキストフィールドに表示されるユーザをwhereで条件検索する。インクリメンタルサーチの時に表示される。
+      @users = User.where.not(id:@ids).where('(name LIKE(?)) and (id != ?)', "%#{params[:keyword]}%", "#{current_user.id}")
+    end
+
     respond_to do |format|
       format.json
     end
